bdb79ffd83ebd8f9e4eabbd8dc873426a879a1ae
Emer Murphy <60451051+emer289@users.noreply.github.com>
2022-10-18 20:34:48 +0100

    assignment requirements working

--- server.js
+++ server.js
@@ -9,8 +9,18 @@ const axios = require("axios")
 const port = 3000;
 
 const path = require('path');
+const {response, query} = require("express");
 
 let publicPath = path.resolve(__dirname, "public");
+//hash map to store weather forecast for each date
+let weatherForecast = {};
+let airQualityForecast = {};
+
+let rainForecasted = "no rain this weekend wahoo!";
+let packCold = null;
+let packWarm = null;
+let packHot = null;
+let needMask = null;
 
 app.use(express.static(publicPath));
 app.use(cors());
@@ -20,67 +30,51 @@ app.get('/', (req, res) => {
 });
 
 //passing location
-app.get("/async/:location", async (req, res) => {
+app.get("/:location", async (req, res) => {
     try {
         const cityName = req.params.location
-        //hash map to store weather forecast for each date
-        let weatherForecast = {};
-        let rainForecasted = "no rain this weekend wahoo!";
-        let packCold = false;
-        let packWarm = false;
-        let packHot = false;
+
+        let lat = null;
+        let lon = null;
 
         await axios("http://api.openweathermap.org/data/2.5/forecast?q="+
             cityName+"&APPID="+process.env.OPEN_WEATHER_KEY +"&units=metric").then(
-            response => {
-                    let weatherList = response.data.list;
-                    for (weatherIndex in weatherList) {
-                        let date = new Date(response.data.list[weatherIndex].dt * 1000);
-                        date.setHours(0, 0, 0, 0);
-                      //date = date.toLocaleDateString();
+            async response => {
+                let weatherList = response.data.list;
+                lat = response.data.city.coord.lat;
+                lon = response.data.city.coord.lon;
 
-                        if (!weatherForecast[date]) {
-                            weatherForecast[date] = {
-                                temperatures: [],
-                                temperatureAverage: null,
-                                windSpeeds: [],
-                                windSpeedsAverage: null,
-                                rainFallLevel: [],
-                                rainFallLevelAverage: null,
+                //function to get Temperature, wind speed and rainfall
+                getWeatherInfo(weatherList)
 
-                            }
-                        }
+                //function to get pm2_5 value
+                await getAirPollutionValue(lon, lat)
 
-                        weatherForecast[date].temperatures.push(weatherList[weatherIndex].main.temp);
-                        //computing the average temp
-                        weatherForecast[date].temperatureAverage = getAverage(weatherForecast[date].temperatures)
-
-                        weatherForecast[date].windSpeeds.push(weatherList[weatherIndex].wind.speed)
-                        weatherForecast[date].windSpeedsAverage = getAverage(weatherForecast[date].windSpeeds)
-
-                        if (weatherList[weatherIndex].rain && weatherList[weatherIndex].rain['3h']) {
-                            rainForecasted = "unfortunately it's raining over the next 4 day, BRING AN UMBRELLA ";
-                            weatherForecast[date].rainForecasted = rainForecasted;
-                            weatherForecast[date].rainFallLevel.push(weatherList[weatherIndex].rain['3h'])
-                            weatherForecast[date].rainFallLevelAverage = (weatherForecast[date].rainFallLevel).reduce((partialSum, a) => partialSum + a, 0)
-                        }
-                        if((weatherForecast[date].temperatures).some(a => a<12) ){
-                            packCold = true;
-                        }else if ((weatherForecast[date].temperatures).some(a => a>12 && a<24) ){
-                            packWarm = true;
-                        }else if((weatherForecast[date].temperatures).some(a => a>24)){
-                            packHot = true;
-                        }
-                    }
-                }
+            }
         );
+
+
 
       res.status(200).json({ weatherForecast: weatherForecast,
           rainForecasted: rainForecasted,
           packCold: packCold,
           packHot: packHot,
-          packWarm: packWarm
+          packWarm: packWarm,
+          airQualityForecast: airQualityForecast,
+          needMask: needMask
+
       });
+
+
+      weatherForecast = {};
+      airQualityForecast = {};
+
+      rainForecasted = "no rain this weekend wahoo!";
+      packCold = null;
+      packWarm = null;
+      packHot = null;
+      needMask = null;
+
 
     } catch (err) {
         res.status(500).json({ message: err });
@@ -88,6 +82,77 @@ app.get("/async/:location", async (req, res) => {
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
+
+async function getAirPollutionValue(lon, lat) {
+    await axios("http://api.openweathermap.org/data/2.5/air_pollution/" +
+        "forecast?lat=" + lat + "&lon=" + lon + "&appid=" + process.env.OPEN_WEATHER_KEY).then(
+        response => {
+            let airQualityList = response.data.list
+            for (airQ in airQualityList) {
+                let date = new Date(airQualityList[airQ].dt * 1000);
+                date.setHours(0, 0, 0, 0);
+                date = date.toLocaleDateString();
+
+                if (!airQualityForecast[date]) {
+                    airQualityForecast[date] = {
+                        pm2_5: [],
+                        pm2_5Average: null,
+                    }
+                }
+
+                airQualityForecast[date].pm2_5.push(airQualityList[airQ].components.pm2_5);
+                if((airQualityForecast[date].pm2_5).some(a => a > 10)){
+                    needMask = "bring a mask"
+                }
+            }
+
+        })
+
+
+    }
+
+function getWeatherInfo(weatherList){
+
+    for (weatherIndex in weatherList) {
+        let date = new Date(weatherList[weatherIndex].dt * 1000);
+        date.setHours(0, 0, 0, 0);
+        date = date.toLocaleDateString();
+
+
+        if (!weatherForecast[date]) {
+            weatherForecast[date] = {
+                temperatures: [],
+                temperatureAverage: null,
+                windSpeeds: [],
+                windSpeedsAverage: null,
+                rainFallLevel: [],
+                rainFallLevelAverage: null,
+            }
+        }
+
+        weatherForecast[date].temperatures.push(weatherList[weatherIndex].main.temp);
+        //computing the average temp
+        weatherForecast[date].temperatureAverage = getAverage(weatherForecast[date].temperatures)
+
+        weatherForecast[date].windSpeeds.push(weatherList[weatherIndex].wind.speed)
+        weatherForecast[date].windSpeedsAverage = getAverage(weatherForecast[date].windSpeeds)
+
+        if (weatherList[weatherIndex].rain && weatherList[weatherIndex].rain['3h']) {
+            rainForecasted = "unfortunately it's raining over the next 4 day, BRING AN UMBRELLA ";
+            weatherForecast[date].rainForecasted = rainForecasted;
+            weatherForecast[date].rainFallLevel.push(weatherList[weatherIndex].rain['3h'])
+            weatherForecast[date].rainFallLevelAverage = (weatherForecast[date].rainFallLevel).reduce((partialSum, a) => partialSum + a, 0)
+        }
+        if((weatherForecast[date].temperatures).some(a => a<12) ){
+            packCold = "pack for COLD weather";
+        }else if ((weatherForecast[date].temperatures).some(a => a>12 && a<24) ){
+            packWarm = "pack for WARM weather";
+        }else if((weatherForecast[date].temperatures).some(a => a>24)){
+            packHot = "pack for HOT weather";
+        }
+    }
+
+}
 
 function getAverage(array){
     return (array.reduce((partialSum, a) => partialSum + a, 0))
